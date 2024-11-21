bea464d6756395c602881379b65d5120a5724fcf
ErickFLM <131476329+ErickFLM@users.noreply.github.com>
2024-11-21 14:18:07 -0300

    feat: Classe de modelagem Telefone

---
+++ assets/php/telefone.php
@@ -0,0 +1,39 @@
+<?php
+//
+include_once 'conectar.php';
+
+
+class Doador{
+    private $ID_doador;
+    private $telefone;
+    private $conn;
+    
+
+    public function getIDDoador()
+    {
+        return $this->ID_doador;
+    }
+
+    public function setIDDoador($ID_doador)
+    {
+        $this->ID_doador = $ID_doador;
+    }
+
+    public function gettelefone()
+    {
+        return $this->telefone;
+    }
+
+    public function setnome($telefone)
+    {
+        $this->telefone = $telefone;
+    }
+
+  
+
+}
+
+
+
+?>
+
